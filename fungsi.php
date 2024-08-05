0cd4c1d0e8f88053a588218f5ade483999627d70
MuhamadAdhiWinata <muhamadadhiw@gmail.com>
2024-08-05 11:49:28 +0700

    l

---
+++ fungsi.php
@@ -0,0 +1,36 @@
+<?php
+
+function tampilkanNomorUrut($maxNumber) {
+    for ($x = 1; $x <= $maxNumber; $x++) {
+        if ($x < $maxNumber) {
+            echo "$x-";
+        } else {
+            echo "$x";
+        }
+    }
+}
+
+function cekBilanganGenapGanjil($bil) {
+    if ($bil % 2 == 0) {
+        return "$bil Merupakan Bilangan Genap";
+    } else {
+        return "$bil Merupakan Bilangan Ganjil";
+    }
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['submitUrutan'])) {
+        $maxNumber = $_POST['maxNumber']; // Mengambil input untuk urutan angka
+        echo "<h2>Hasil Urutan Angka</h2>";
+        echo "<p>";
+        tampilkanNomorUrut($maxNumber);
+        echo "</p>";
+    }
+
+    if (isset($_POST['submitCek'])) {
+        $bil = $_POST['bil']; // Mengambil input untuk cek bilangan
+        echo "<h2>Hasil Cek Bilangan</h2>";
+        echo "<p>" . cekBilanganGenapGanjil($bil) . "</p>";
+    }
+}
+?>
---
+++ no3.php
@@ -0,0 +1,124 @@
+<?php
+
+function isVocal($letter){
+    if(in_array(strtolower($letter), array("a", "i", "u", "e", "o"))){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function isAlpha($letter){
+    if(preg_match("/[a-zA-Z]/", $letter)){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+class Stack {
+    
+    var $data = "";
+    
+    public function push($letter){
+        $this->data .= $letter;
+    }
+    
+    public function pop(){
+        if(strlen($this->data) > 0){
+            $temp = $this->data;
+            $this->data = substr($this->data, 0, -1);
+            return $temp[strlen($temp) - 1];
+        } else {
+            return "";
+        }
+    }
+    
+    public function top(){
+        if(strlen($this->data) > 0){
+            $temp = $this->data;
+            return $temp[strlen($temp) - 1];
+        } else {
+            return "";
+        } 
+    }
+    
+    public function show(){
+        echo $this->data;
+    }
+    
+    public function popAll(){
+        $temp = $this->data;
+        $this->data = "";
+        return $temp;
+    }
+}
+
+//ha  na ca ra ka  da ta sa  wa la
+//pa dha ja ya nya ma ga ba tha nga
+
+function convert($text){
+    $dict = array(
+        "h" => "p",
+        "n" => "dh",
+        "c" => "j",
+        "r" => "y",
+        "k" => "ny",
+        "d" => "m",
+        "t" => "g",
+        "s" => "b",
+        "w" => "th",
+        "l" => "ng",
+        "p" => "h",
+        "dh" => "n",
+        "j" => "c",
+        "y" => "r",
+        "ny" => "k",
+        "m" => "d",
+        "g" => "t",
+        "b" => "s",
+        "th" => "w",
+        "ng" => "l"
+    );
+    if(in_array(strtolower($text), $dict)){
+        return $dict[strtolower($text)];
+    } else {
+        $tmp = str_split($text);
+        $ret = "";
+        foreach($tmp as $t){
+            if(in_array(strtolower($t), $dict)){
+                $ret .= $dict[strtolower($t)];
+            } else {
+                $ret .= $t;
+            }
+        }
+        return $ret;
+    }
+}
+
+$string = $_GET["text"];
+
+$stack = new Stack();
+
+$result = "";
+
+$items = str_split($string);
+foreach($items as $item){
+    if(isAlpha($item)){
+        if(isVocal($item)){
+            $result .= convert($stack->popAll()) . $item;
+        } else if($item == strtolower("h")){
+            $stack->push($item);
+            $result .= convert($stack->popAll());
+        } else {
+            $result .= $stack->push($item);
+        }
+    } else {
+        $result .= convert($stack->popAll()) . $item;
+    }
+}
+
+$result .= convert($stack->popAll());
+
+
+echo "Hasil Konversi adalah = $result";
